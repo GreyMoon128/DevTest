3f4f86e81155034851f8035ddb1108360fdbc956
GreyMoon128 <165284273+GreyMoon128@users.noreply.github.com>
2024-04-03 09:31:15 -0400

    Update main.py

--- ZachColab/main.py
+++ ZachColab/main.py
@@ -1,5 +1,10 @@
 #Dev Test 1
+<<<<<<< Updated upstream
 https://www.wikipedia.org?q=python&l=English
 #Zach, what the fuck am I doing to not change the fucking file>?
 #commit
 git push
+=======
+URL=https://www.wikipedia.org?q=python&l=English
+#Zach, what the fuck am I doing to not change the fucking file>?
+>>>>>>> Stashed changes
